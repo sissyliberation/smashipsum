6d8d30a345e7057c42fed7381ebf33162bd4064c
nova <courtois1337@gmail.com>
2016-09-05 04:19:34 -0500

    actual melee words

--- server.js
+++ server.js
@@ -8,19 +8,30 @@ var port = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
 
+
+// console.log();
+
+var ssbmDictionary = require(__dirname+'/lib/ssbm/dictionary');
+
+console.log(ssbmDictionary);
+
+var tmp = [];
+var words = tmp.concat(ssbmDictionary.characters, ssbmDictionary.stages, ssbmDictionary.items, ssbmDictionary.general);
+
+
 app.get('/api/get/', function(req, res) {
     var numUnits =  req.query.numUnits || 3;
 
     var output = loremIpsum({
         count:  numUnits,             // Number of words, sentences, or paragraphs to generate.
-        units: 'paragraphs',            // Generate words, sentences, or paragraphs.
-        sentenceLowerBound: 5,         // Minimum words per sentence.
-        sentenceUpperBound: 15,        // Maximum words per sentence.
+        units: 'paragraphs',          // Generate words, sentences, or paragraphs.
+        sentenceLowerBound: 5,        // Minimum words per sentence.
+        sentenceUpperBound: 15,       // Maximum words per sentence.
         paragraphLowerBound: 3,       // Minimum sentences per paragraph.
-        paragraphUpperBound: 7,        // Maximum sentences per paragraph.
+        paragraphUpperBound: 7,       // Maximum sentences per paragraph.
         format: 'html',               // Plain text or html
-        // words: ['ad', 'dolor', ... ]  // Custom word dictionary. Uses dictionary.words (in lib/dictionary.js) by default.
-        random: Math.random          // A PRNG function. Uses Math.random by default
+        words: words,                 // Custom word dictionary. Uses dictionary.words (in lib/dictionary.js) by default.
+        random: Math.random           // A PRNG function. Uses Math.random by default
     });
 
     res.send(output);
