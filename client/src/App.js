87b167e3378083166bb98bbade6ed18540ac3672
nova <courtois1337@gmail.com>
2019-08-24 10:06:05 -0500

    add base ipsum component

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,9 @@ import './app.scss';
 
 import { Layout } from 'antd';
 import Settings  from './components/settings';
+import Ipsum  from './components/ipsum';
 
 const { Header, Footer, Content } = Layout;
-
-
 
 class App extends React.Component {
   constructor(props) {
@@ -25,6 +24,7 @@ class App extends React.Component {
         <Header>header</Header>
         <Content>
           <Settings {...this.state} />
+          <Ipsum {...this.state} />
         </Content>
         <Footer>footer</Footer>
       </Layout>
