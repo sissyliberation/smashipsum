0336ab13feb94e89aa022e69e681274e35a39e80
nova <courtois1337@gmail.com>
2020-11-24 11:47:48 -0800

    update footer theme styling

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTheme } from '../../ThemeContext';
 import { Layout } from 'antd';
 
 import './style.scss';
@@ -6,8 +7,9 @@ import './style.scss';
 const { Footer } = Layout;
 
 export default function FooterContent(props) {
+  const { themeClass } = useTheme();
   return (
-    <Footer className="footer">
+    <Footer className={`footer ${themeClass}`}>
       <div className="footer__container">
         // Created in ATX by <a href="https://novacourtois.github.io" target="_blank" rel="noopener noreferrer">Nova Skye</a>
       </div>
